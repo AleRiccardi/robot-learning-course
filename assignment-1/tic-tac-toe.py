7114b131594d858a90dbd3b8acbb9e86ced82b9e
Ilyass Taouil <itaouil95@gmail.com>
2020-04-23 18:06:43 +0200

    Bella.

--- assignment-1/tic-tac-toe.py
+++ assignment-1/tic-tac-toe.py
@@ -1,15 +1,9 @@
-
 import numpy as np
 
 
 def move_still_possible(S):
     return not (S[S==0].size == 0)
 
-def move(S, p):
-    if p == 1:
-        return move_by_selection(S)
-    else:
-        return move_at_random(S)
 
 def move_at_random(S):
     xs, ys = np.where(S==0)
@@ -34,7 +28,8 @@ def move_by_selection(S):
             valid = True
         else:
             print("Invalid move")
-    return S
+
+    return S, i
     
 
 def move_was_winning_move(S, p):
@@ -53,7 +48,6 @@ def move_was_winning_move(S, p):
     return False
 
 
-
 # python dictionary to map integers (1, -1, 0) to characters ('x', 'o', ' ')
 symbols = { 1:'x',
            -1:'o',
@@ -68,9 +62,29 @@ def print_game_state(S):
     print (B)
 
 
+def updateVs(player, lastMove, vs):
+    # Tracker length
+    n_0 = len(tracker) - 1
+
+    # Temporal V(s) for terminal state
+    terminalVs = np.ones() * 0.1
+
+    # Traverse tracker in reverse order
+    for x in range(n_0, -1, -1):
+        # Update terminal state V(s)
+        if (x == n_0):
+            vs[tracker[x]][lastMove] = 1 if player == 1 else 0
+        
+        # Update previous states V(s)
+        vs[tracker[x]] = vs[tracker[x]] + 0.2 * (vs[tracker[x+1]] - vs[tracker[x]])
+
+
 if __name__ == '__main__':
     # initialize an empty tic tac toe board
     gameState = np.zeros((3,3), dtype=int)
+
+    # Last player before terminal state
+    lastMove = None
 
     # initialize the player who moves first (either +1 or -1)
     player = 1
@@ -80,6 +94,16 @@ if __name__ == '__main__':
 
     # initialize a flag that indicates whetehr or not game has ended
     noWinnerYet = True
+
+    # V(s) hash-table
+    vs = dict()
+
+    # State tracker
+    tracker = []
+
+    # Initialize vs with state0 V(s)
+    vs[gameState] = np.ones(9) * 0.1
+    tracker.append(gameState)
     
     while move_still_possible(gameState) and noWinnerYet:
         # turn current player number into player symbol
@@ -87,7 +111,14 @@ if __name__ == '__main__':
         print ('%s moves' % name)
 
         # let current player move at random
-        gameState = move(gameState, player)
+        if player == 1:
+            gameState, lastMove = move_by_selection(S)
+        else:
+            gameState = move_at_random(S)
+
+        # Add successor state to V(s)
+        vs[gameState] = np.ones(9) * 0.1
+        tracker.append(gameState)
 
         # print current game state
         print_game_state(gameState)
@@ -100,8 +131,9 @@ if __name__ == '__main__':
         # switch current player and increase move counter
         player *= -1
         mvcntr +=  1
-
-
+    
+    # Update Vs
+    updateVs(player, lastMove, vs)
 
     if noWinnerYet:
         print ('game ended in a draw' )
