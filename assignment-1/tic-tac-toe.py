9aa556399c54dad2f6f58ded0d3a37e5dbadbe5c
Alessandro <alericcardi1996@gmail.com>
2020-04-23 19:23:13 +0200

    Added AI

--- assignment-1/tic-tac-toe.py
+++ assignment-1/tic-tac-toe.py
@@ -5,7 +5,7 @@ def move_still_possible(S):
     return not (S[S==0].size == 0)
 
 
-def move_at_random(S):
+def move_o(S):
     xs, ys = np.where(S==0)
 
     i = np.random.permutation(np.arange(xs.size))[0]
@@ -14,20 +14,45 @@ def move_at_random(S):
 
     return S
 
-def move_by_selection(S):
+def move_x(S, vs, count):
     xs, ys = np.where(S==0)
     valid = False
-    while(not valid):
-        print("Make a move: ", end="")
-        i = int(input()) - 1
-        x = i % 3
-        y = i // 3
+    
+    # first 10 games, play by selection
+    if count < 1:
+        while(not valid):
+            print("Make a move: ", end="")
+            i = int(input()) - 1
+            x = i % 3
+            y = i // 3
 
-        if S[y, x] == 0:
-            S[y, x] = 1
-            valid = True
+            if S[y, x] == 0:
+                S[y, x] = 1
+                valid = True
+            else:
+                print("Invalid move")
+    
+    # play by AI
+    else:
+        # AI choice
+        if np.random.random_sample((0,1)) > 0.1:
+            vs_values = vs[np.array_str(S)]
+            valid_poses = [ys[i] * 3 + xs[i] for i in range(xs.size)]
+
+            max_v = -np.inf
+            i = -1
+            for k in range(vs_values.size):
+                if (vs_values[k] > max_v and k in valid_poses):
+                    max_v = vs_values[k]
+                    i = k
+
+            S[xs[i],ys[i]] = 1
+
+        # Rand choice
         else:
-            print("Invalid move")
+            i = np.random.permutation(np.arange(xs.size))[0]
+            S[xs[i],ys[i]] = 1
+
 
     return S, i
     
@@ -62,78 +87,91 @@ def print_game_state(S):
     print (B)
 
 
-def updateVs(player, lastMove, vs):
+def updateVs(vs, tracker, lastMove, last_vs):
     # Tracker length
     n_0 = len(tracker) - 1
-
-    # Temporal V(s) for terminal state
-    terminalVs = np.ones() * 0.1
 
     # Traverse tracker in reverse order
     for x in range(n_0, -1, -1):
         # Update terminal state V(s)
         if (x == n_0):
-            vs[tracker[x]][lastMove] = 1 if player == 1 else 0
+            vs[np.array_str(tracker[x])][lastMove] = last_vs
+            continue
         
         # Update previous states V(s)
-        vs[tracker[x]] = vs[tracker[x]] + 0.2 * (vs[tracker[x+1]] - vs[tracker[x]])
+        vs[np.array_str(tracker[x])] = (vs[np.array_str(tracker[x])] + 0.2 * (vs[np.array_str(tracker[x+1])] - vs[np.array_str(tracker[x])]))
 
+
+def showStatistics(wons, count):
+    wons_np = np.array(wons)
 
 if __name__ == '__main__':
-    # initialize an empty tic tac toe board
+    # First game state initialization
     gameState = np.zeros((3,3), dtype=int)
-
-    # Last player before terminal state
-    lastMove = None
-
-    # initialize the player who moves first (either +1 or -1)
-    player = 1
-
-    # initialize a move counter
-    mvcntr = 1
-
-    # initialize a flag that indicates whetehr or not game has ended
-    noWinnerYet = True
-
     # V(s) hash-table
-    vs = dict()
-
-    # State tracker
-    tracker = []
-
-    # Initialize vs with state0 V(s)
-    vs[gameState] = np.ones(9) * 0.1
-    tracker.append(gameState)
+    vs = {np.array_str(gameState): np.ones(9) * 0.1}
     
-    while move_still_possible(gameState) and noWinnerYet:
-        # turn current player number into player symbol
-        name = symbols[player]
-        print ('%s moves' % name)
+    # Game variable
+    countGame = 0
+    wons = []
 
-        # let current player move at random
-        if player == 1:
-            gameState, lastMove = move_by_selection(S)
-        else:
-            gameState = move_at_random(S)
+    while(countGame < 1010):
+        # initialize an empty tic tac toe board
+        gameState = np.zeros((3,3), dtype=int)
+        # Last player before terminal state
+        lastMove = None
+        last_vs = 0
 
-        # Add successor state to V(s)
-        vs[gameState] = np.ones(9) * 0.1
+        # initialize the player who moves first (either +1 or -1)
+        player = 1
+
+        # initialize a move counter
+        mvcntr = 1
+
+        # initialize a flag that indicates whetehr or not game has ended
+        noWinnerYet = True
+
+        # State tracker
+        tracker = []
+
+        # Initialize vs with state0 V(s)
         tracker.append(gameState)
+        while move_still_possible(gameState) and noWinnerYet:
+            # turn current player number into player symbol
+            name = symbols[player]
+            print ('%s moves' % name)
 
-        # print current game state
-        print_game_state(gameState)
+            # let current player move at random
+            if player == 1:
+                gameState, lastMove = move_x(gameState, vs, countGame)
+            else:
+                gameState = move_o(gameState)
+
+            # Check if the game state doesn't exists
+            if np.array_str(gameState) not in vs.keys():
+                vs[np.array_str(gameState)] = np.ones(9) * 0.1
+            
+            # Add successor state to V(s)
+            tracker.append(gameState)
+
+            # print current game state
+            print_game_state(gameState)
+            
+            # evaluate current game state
+            if move_was_winning_move(gameState, player):
+                print ('player %s wins after %d moves' % (name, mvcntr))
+                noWinnerYet = False
+                last_vs = 1 if player == 1 else 0
+
+            # switch current player and increase move counter
+            player *= -1
+            mvcntr +=  1
+
+        showStatistics(wons, countGame)
+        countGame += 1
         
-        # evaluate current game state
-        if move_was_winning_move(gameState, player):
-            print ('player %s wins after %d moves' % (name, mvcntr))
-            noWinnerYet = False
-
-        # switch current player and increase move counter
-        player *= -1
-        mvcntr +=  1
-    
     # Update Vs
-    updateVs(player, lastMove, vs)
+    updateVs(vs, tracker, lastMove, last_vs)
 
     if noWinnerYet:
         print ('game ended in a draw' )
