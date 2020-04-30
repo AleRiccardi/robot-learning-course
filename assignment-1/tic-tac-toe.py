a49b9a044315d14dd0a67acff50cf62c7d08833d
Alessandro <alericcardi1996@gmail.com>
2020-04-30 16:07:50 +0200

    Corrected ex

--- assignment-1/tic-tac-toe.py
+++ assignment-1/tic-tac-toe.py
@@ -1,191 +1,262 @@
+import signal
+import time
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""Task 1.1
+ -> Implementation of the GAME
+"""
+class TicTacToe:
+    # Game params
+    continue_to_play = True
+    count_games = 0
+    count_wins = []
+    symbols = {1: 'x',
+               -1: 'o',
+               0: ' '}
 
-def move_still_possible(S):
-    return not (S[S==0].size == 0)
+    USER_GAMES = 1
+    AI_GAMES = 1000
+    # Root game
+    ROOT_GAME = np.zeros((3, 3), dtype=int)
+    # Init game
+    game_state = ROOT_GAME
+    # V(s) hash-table
+    V_s = {np.array_str(game_state): np.ones(9) * 0.1}
 
+    # Other params
+    original_sigint = None
 
-def move_o(S):
-    xs, ys = np.where(S==0)
+    def __init__(self):
+        # store the original SIGINT handler
+        self.original_sigint = signal.getsignal(signal.SIGINT)
+        signal.signal(signal.SIGINT, self.exit_gracefully)
 
-    i = np.random.permutation(np.arange(xs.size))[0]
-    
-    S[xs[i],ys[i]] = -1
+    def exit_gracefully(self, signum, frame):
+        # restore the original signal handler as otherwise evil things will happen
+        # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
+        signal.signal(signal.SIGINT, self.original_sigint)
 
-    return S
+        try:
+            if input("\nReally quit? (y/n)> ").lower().startswith('y'):
+                self.continue_to_play = False
 
-def move_x(S, vs, count):
-    xs, ys = np.where(S==0)
-    valid = False
-    
-    # first 10 games, play by selection
-    if count < 10:
-        while(not valid):
-            print("Make a move: ", end="")
-            i = int(input()) - 1
-            x = i % 3
-            y = i // 3
+        except KeyboardInterrupt:
+            print("Ok ok, quitting")
+            sys.exit(1)
 
-            if S[y, x] == 0:
-                S[y, x] = 1
-                valid = True
-            else:
-                print("Invalid move")
-    
-    # play by AI
-    else:
-        # AI choice
-        if np.random.random_sample((0,1)) > 0.1:
-            vs_values = vs[np.array_str(S)]
-            valid_poses = [ys[i] * 3 + xs[i] for i in range(xs.size)]
+        # restore the exit gracefully handler here
+        signal.signal(signal.SIGINT, self.exit_gracefully)
 
-            max_v = -np.inf
-            i = -1
-            for k in range(vs_values.size):
-                if (vs_values[k] > max_v and k in valid_poses):
-                    max_v = vs_values[k]
-                    i = k
+    def move_still_possible(self, current_game):
+        return not (current_game[current_game == 0].size == 0)
 
-            S[xs[i],ys[i]] = 1
+    def move_o(self, current_game):
+        ys, xs = np.where(current_game == 0)
+        # generate random movement
+        i = np.random.permutation(np.arange(ys.size))[0]
+        # save movement
+        current_game[ys[i], xs[i]] = -1
 
-        # Rand choice
+        return current_game
+
+    def move_x(self, current_game, count):
+        ys, xs = np.where(current_game == 0)
+        valid = False
+
+        # first 10 games, play by selection
+        if count < self.USER_GAMES:
+            while(not valid):
+                value = -1
+                try:
+                    value = int(input("Make a move: "))
+                except Exception as e:
+                    print()
+                    print("Please insert a number")
+                    continue
+
+                if value not in range(1, 10):
+                    print("Please insert a number between 1 and 9")
+                    continue
+
+                i = value - 1
+                x = i % 3
+                y = i // 3
+
+                if current_game[y, x] == 0:
+                    current_game[y, x] = 1
+                    valid = True
+                else:
+                    valid_poses = [(ys[i] * 3 + xs[i]) +
+                                   1 for i in range(ys.size)]
+                    print("Invalid move, the valid poses are:")
+                    print(sorted(valid_poses))
+
+        # played by AI
         else:
-            i = np.random.permutation(np.arange(xs.size))[0]
-            S[xs[i],ys[i]] = 1
+            # AI choice
+            # Task 1.4 - Automated procedure selection
+            if np.random.random_sample(1) > 0.1:
+                V_s_values = self.V_s[np.array_str(current_game)]
+                valid_poses = [ys[i] * 3 + xs[i] for i in range(ys.size)]
 
+                max_v = -np.inf
+                i = -1
 
-    return S, i
-    
+                for k in range(V_s_values.size):
+                    if (V_s_values[k] > max_v and k in valid_poses):
+                        max_v = V_s_values[k]
+                        i = valid_poses.index(k)
 
-def move_was_winning_move(S, p):
-    if np.max((np.sum(S, axis=0)) * p) == 3:
-        return True
+                current_game[ys[i], xs[i]] = 1
 
-    if np.max((np.sum(S, axis=1)) * p) == 3:
-        return True
+            # Rand choice
+            else:
+                i = np.random.permutation(np.arange(ys.size))[0]
+                current_game[ys[i], xs[i]] = 1
 
-    if (np.sum(np.diag(S)) * p) == 3:
-        return True
+        return current_game, i
 
-    if (np.sum(np.diag(np.rot90(S))) * p) == 3:
-        return True
+    def move_was_winning_move(self, current_game, player):
+        if np.max((np.sum(current_game, axis=0)) * player) == 3:
+            return True
 
-    return False
+        if np.max((np.sum(current_game, axis=1)) * player) == 3:
+            return True
 
+        if (np.sum(np.diag(current_game)) * player) == 3:
+            return True
 
-# python dictionary to map integers (1, -1, 0) to characters ('x', 'o', ' ')
-symbols = { 1:'x',
-           -1:'o',
-            0:' '}
+        if (np.sum(np.diag(np.rot90(current_game))) * player) == 3:
+            return True
 
+        return False
 
-# print game state matrix using characters
-def print_game_state(S):
-    B = np.copy(S).astype(object)
-    for n in [-1, 0, 1]:
-        B[B==n] = symbols[n]
-    print (B)
+    # print game state matrix using characters
 
+    def print_game_state(self, current_game):
+        B = np.copy(current_game).astype(object)
+        for n in [-1, 0, 1]:
+            B[B == n] = self.symbols[n]
+        print(B)
 
-def updateVs(vs, tracker, lastMove, last_vs):
-    # Tracker length
-    n_0 = len(tracker) - 1
+    def learning_algo(self, tracker, last_move, last_score):
+        # Tracker length
+        n_movements = len(tracker) - 1
 
-    # Traverse tracker in reverse order
-    for x in range(n_0, -1, -1):
-        # Update terminal state V(s)
-        if (x == n_0):
-            vs[np.array_str(tracker[x])][lastMove] = last_vs
-            continue
-        
-        # Update previous states V(s)
-        vs[np.array_str(tracker[x])] = (vs[np.array_str(tracker[x])] + 0.2 * (vs[np.array_str(tracker[x+1])] - vs[np.array_str(tracker[x])]))
+        # Traverse tracker in reverse order
+        for x in range(n_movements, -1, -1):
+            # Update terminal state V(s)
+            if (x == n_movements):
+                self.V_s[np.array_str(tracker[x])][last_move] = last_score
+                continue
+            
+            theta = max(min(((9 - x) / 6) * 0.2, 0.2), 0.05)
 
+            # Update previous states V(s)
+            # Task 1.3 - Implementation of the update formula
+            self.V_s[np.array_str(tracker[x])] = (self.V_s[np.array_str(
+                tracker[x])] + theta * (self.V_s[np.array_str(tracker[x+1])] - self.V_s[np.array_str(tracker[x])]))
 
-def showStatistics(wins, count):
-    freq = []
-    for x in range(0, len(wins), 100):
-        freq.append(wins[x-100:x].count(1) / 100)
+    def show_statistics(self):
+        freq = []
+        for x in range(0, len(self.count_wins), 100):
+            freq.append(self.count_wins[x-100:x].count(1) / 100)
 
-    plt.plot(freq)
-    plt.ylabel('Win frequency')
-    plt.show()
+        plt.plot(freq)
+        plt.ylabel('Win ratio')
+        plt.show()
+
+    def run(self):
+
+        # First game state initialization
+        self.game_state = np.zeros((3, 3), dtype=int)
+        # V(s) hash-table
+        self.V_s = {np.array_str(self.game_state): np.ones(9) * 0.1}
+
+        # Game variable
+        self.count_games = 0
+        self.count_wins = []
+
+        while(self.continue_to_play):
+
+            if self.count_games > self.AI_GAMES + self.USER_GAMES:
+                self.continue_to_play = False
+                continue
+
+            # initialize an empty tic tac toe board
+            self.game_state = np.zeros((3, 3), dtype=int)
+            # Last player before terminal state
+            last_move = None
+            last_score = 0
+
+            # initialize the player who moves first (either +1 or -1)
+            player = 1
+
+            # initialize a move counter
+            mvcntr = 1
+
+            # initialize a flag that indicates whetehr or not game has ended
+            noWinnerYet = True
+
+            # State tracker
+            tracker = []
+
+            # Initialize self.V_s with state0 V(s)
+            tracker.append(self.game_state)
+            while self.move_still_possible(self.game_state) and noWinnerYet:
+                # turn current player number into player symbol
+                name = self.symbols[player]
+                print('%s moves' % name)
+
+                # let current player move at random
+                if player == 1:
+                    self.game_state, last_move = self.move_x(
+                        self.game_state, self.count_games)
+                else:
+                    self.game_state = self.move_o(self.game_state)
+
+                # Check if the game state doesn't exists
+                # Task 1.2 - Creation of the game state ENCODING
+                # -> np.array_str(self.game_state)
+                if np.array_str(self.game_state) not in self.V_s.keys():
+                    # Task 1.3 - Initialize for V(s) for all states with 0.1
+                    self.V_s[np.array_str(self.game_state)] = np.ones(9) * 0.1
+
+                # Add successor state to V(s)
+                tracker.append(self.game_state)
+
+                # print current game state
+                self.print_game_state(self.game_state)
+
+                # evaluate current game state
+                if self.move_was_winning_move(self.game_state, player):
+                    print('player %s self.count_wins after %d moves' %
+                          (name, mvcntr))
+                    noWinnerYet = False
+                    last_score = 1 if player == 1 else 0
+
+                # switch current player and increase move counter
+                player *= -1
+                mvcntr += 1
+
+            # Update self.V_s
+            self.learning_algo(tracker, last_move, last_score)
+
+            # Update self.count_wins when GameAI plays
+            if (self.count_games > 9):
+                self.count_wins.append(last_score)
+
+            # Increase game counter
+            self.count_games += 1
+
+        if noWinnerYet:
+            print('game ended in a draw')
+
+        self.show_statistics()
 
 
 if __name__ == '__main__':
-    # First game state initialization
-    gameState = np.zeros((3,3), dtype=int)
-    # V(s) hash-table
-    vs = {np.array_str(gameState): np.ones(9) * 0.1}
-    
-    # Game variable
-    countGame = 0
-    wins = []
-
-    while(countGame < 1010):
-        # initialize an empty tic tac toe board
-        gameState = np.zeros((3,3), dtype=int)
-        # Last player before terminal state
-        lastMove = None
-        last_vs = 0
-
-        # initialize the player who moves first (either +1 or -1)
-        player = 1
-
-        # initialize a move counter
-        mvcntr = 1
-
-        # initialize a flag that indicates whetehr or not game has ended
-        noWinnerYet = True
-
-        # State tracker
-        tracker = []
-
-        # Initialize vs with state0 V(s)
-        tracker.append(gameState)
-        while move_still_possible(gameState) and noWinnerYet:
-            # turn current player number into player symbol
-            name = symbols[player]
-            print ('%s moves' % name)
-
-            # let current player move at random
-            if player == 1:
-                gameState, lastMove = move_x(gameState, vs, countGame)
-            else:
-                gameState = move_o(gameState)
-
-            # Check if the game state doesn't exists
-            if np.array_str(gameState) not in vs.keys():
-                vs[np.array_str(gameState)] = np.ones(9) * 0.1
-            
-            # Add successor state to V(s)
-            tracker.append(gameState)
-
-            # print current game state
-            print_game_state(gameState)
-            
-            # evaluate current game state
-            if move_was_winning_move(gameState, player):
-                print ('player %s wins after %d moves' % (name, mvcntr))
-                noWinnerYet = False
-                last_vs = 1 if player == 1 else 0
-
-            # switch current player and increase move counter
-            player *= -1
-            mvcntr +=  1
-
-        # Update Vs
-        updateVs(vs, tracker, lastMove, last_vs)
-
-        # Update wins when GameAI plays
-        if (countGame > 9):
-            wins.append(last_vs)
-
-        # Increase game counter
-        countGame += 1
-
-    if noWinnerYet:
-        print ('game ended in a draw' )
-
-    showStatistics(wins, countGame)
+    game = TicTacToe()
+    game.run()
